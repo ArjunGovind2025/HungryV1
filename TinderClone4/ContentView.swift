08ac8eeb1631b3ba7186ab1fe88703486a68c4f0
Arjun Govind <arjgov1238@gmail.com>
2023-06-30 23:58:00 -0400

    Initial Commit

---
+++ TinderClone4/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TinderClone4
+//
+//  Created by Arjun Govind on 6/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TinderClone4/TinderClone4App.swift
@@ -0,0 +1,17 @@
+//
+//  TinderClone4App.swift
+//  TinderClone4
+//
+//  Created by Arjun Govind on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct TinderClone4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
